a8ed9b42de950c20b1cabd6a14172cefb79af5e6
shubham.meshram <shubham.meshram>
2023-01-15 12:47:10 +0000

    updates to bronze

--- 04 - ETL with Spark SQL/POC_Bronze Layer.sql
+++ 04 - ETL with Spark SQL/POC_Bronze Layer.sql
@@ -49,11 +49,6 @@ CREATE DATABASE IF NOT EXISTS gold_db_retail;
 
 -- COMMAND ----------
 
---show databases;
-describe database bronze_db_retail;
-
--- COMMAND ----------
-
 -- MAGIC %python
 -- MAGIC #View list of datasets 
 -- MAGIC 
@@ -75,105 +70,104 @@ describe database bronze_db_retail;
 
 --all bronze schema table creation statements are defined here: PART 1
 --1
--- CREATE OR REPLACE TABLE bronze_db_retail.active_promotions AS SELECT * FROM parquet.`dbfs:/databricks-datasets/retail-org/active_promotions/active_promotions.parquet`;
+CREATE OR REPLACE TABLE bronze_db_retail.active_promotions AS SELECT * FROM parquet.`dbfs:/databricks-datasets/retail-org/active_promotions/active_promotions.parquet`;
 
 --2
--- drop table if exists bronze_db_retail.company_employees;
--- drop view if exists company_employees;
--- CREATE OR REPLACE TEMP VIEW company_employees
--- USING CSV
--- OPTIONS (
---   path "dbfs:/mnt/dbacademy-datasets/data-engineering-with-databricks/v02/retail-org/company_employees/company_employees.csv",
---   header  "true");
--- --LOCATION 
--- CREATE TABLE bronze_db_retail.company_employees AS
---   SELECT * FROM company_employees;
+drop table if exists bronze_db_retail.company_employees;
+drop view if exists company_employees;
+CREATE OR REPLACE TEMP VIEW company_employees
+USING CSV
+OPTIONS (
+  path "dbfs:/mnt/dbacademy-datasets/data-engineering-with-databricks/v02/retail-org/company_employees/company_employees.csv",
+  header  "true");
+--LOCATION 
+CREATE TABLE bronze_db_retail.company_employees AS
+  SELECT * FROM company_employees;
 
 --3
--- drop table bronze_db_retail.customers;
--- CREATE table bronze_db_retail.customers
--- USING CSV
--- OPTIONS (
---   header = "true")
--- LOCATION "dbfs:/databricks-datasets/retail-org/customers/customers.csv"
+drop table if exists bronze_db_retail.customers;
+CREATE table bronze_db_retail.customers
+USING CSV
+OPTIONS (
+  header = "true")
+LOCATION "dbfs:/databricks-datasets/retail-org/customers/customers.csv";
 
 
 --4 (where schema cannot be infered, create view and then a table from view)
--- drop table if exists bronze_db_retail.loyalty_segments;
--- drop view if exists loyalty_segments;
--- CREATE OR REPLACE TEMP VIEW loyalty_segments
--- --(loyalty_segment_id string, loyalty_segment_description string, unit_threshold string, valid_from string, valid_to string )
--- USING CSV
--- OPTIONS (
---   path "dbfs:/mnt/dbacademy-datasets/data-engineering-with-databricks/v02/retail-org/loyalty_segments/loyalty_segment.csv",
---   header "true"
---   );
--- CREATE TABLE bronze_db_retail.loyalty_segments AS
---   SELECT * FROM loyalty_segments;
+CREATE OR REPLACE TEMP VIEW loyalty_segments
+--(loyalty_segment_id string, loyalty_segment_description string, unit_threshold string, valid_from string, valid_to string )
+USING CSV
+OPTIONS (
+  path "dbfs:/mnt/dbacademy-datasets/data-engineering-with-databricks/v02/retail-org/loyalty_segments/loyalty_segment.csv",
+  header "true"
+  );
+drop table if exists bronze_db_retail.loyalty_segments;
+CREATE TABLE bronze_db_retail.loyalty_segments AS
+  SELECT * FROM loyalty_segments;
 
 -- COMMAND ----------
 
 --all bronze schema table creation statements are defined here: PART 2
 
 --5 (where schema cannot be infered, create view and then a table from view)
--- drop table if exists bronze_db_retail.products;
--- drop view if exists products;
--- CREATE OR REPLACE TEMP VIEW products
--- --(loyalty_segment_id string, loyalty_segment_description string, unit_threshold string, valid_from string, valid_to string )
--- USING CSV
--- OPTIONS (
---   path "dbfs:/mnt/dbacademy-datasets/data-engineering-with-databricks/v02/retail-org/products/products.csv",
---   header "true",
---   delimiter ";"
---   );
--- CREATE TABLE bronze_db_retail.products AS
---   SELECT * FROM products;
+drop table if exists bronze_db_retail.products;
+drop view if exists products;
+CREATE OR REPLACE TEMP VIEW products
+--(loyalty_segment_id string, loyalty_segment_description string, unit_threshold string, valid_from string, valid_to string )
+USING CSV
+OPTIONS (
+  path "dbfs:/mnt/dbacademy-datasets/data-engineering-with-databricks/v02/retail-org/products/products.csv",
+  header "true",
+  delimiter ";"
+  );
+CREATE TABLE bronze_db_retail.products AS
+  SELECT * FROM products;
   
   
 --6 (where schema cannot be infered, create view and then a table from view)
--- drop table if exists bronze_db_retail.promotions;
--- drop view if exists promotions;
--- CREATE OR REPLACE TEMP VIEW promotions
--- --(loyalty_segment_id string, loyalty_segment_description string, unit_threshold string, valid_from string, valid_to string )
--- USING CSV
--- OPTIONS (
---   path "dbfs:/mnt/dbacademy-datasets/data-engineering-with-databricks/v02/retail-org/promotions/promotions.csv",
---   header "true"
---   --delimiter ";"
---   );
--- CREATE TABLE bronze_db_retail.promotions AS
---   SELECT * FROM promotions;
+drop table if exists bronze_db_retail.promotions;
+drop view if exists promotions;
+CREATE OR REPLACE TEMP VIEW promotions
+--(loyalty_segment_id string, loyalty_segment_description string, unit_threshold string, valid_from string, valid_to string )
+USING CSV
+OPTIONS (
+  path "dbfs:/mnt/dbacademy-datasets/data-engineering-with-databricks/v02/retail-org/promotions/promotions.csv",
+  header "true"
+  --delimiter ";"
+  );
+CREATE TABLE bronze_db_retail.promotions AS
+  SELECT * FROM promotions;
 
 --7
--- drop table if exists bronze_db_retail.purchase_orders;
--- CREATE TABLE bronze_db_retail.purchase_orders
--- USING xml
--- OPTIONS (path "dbfs:/mnt/dbacademy-datasets/data-engineering-with-databricks/v02/retail-org/purchase_orders/purchase_orders.xml", rowTag "purchase_item", inferSchema "true")
+drop table if exists bronze_db_retail.purchase_orders;
+CREATE TABLE bronze_db_retail.purchase_orders
+USING xml
+OPTIONS (path "dbfs:/mnt/dbacademy-datasets/data-engineering-with-databricks/v02/retail-org/purchase_orders/purchase_orders.xml", rowTag "purchase_item", inferSchema "true");
 
 --8
--- drop table if exists bronze_db_retail.sales_orders;
--- CREATE TABLE bronze_db_retail.sales_orders as
--- select * from json.`dbfs:/databricks-datasets/retail-org/sales_orders/`;
+drop table if exists bronze_db_retail.sales_orders;
+CREATE TABLE bronze_db_retail.sales_orders as
+select * from json.`dbfs:/databricks-datasets/retail-org/sales_orders/`;
 
 
 --9
--- drop table if exists bronze_db_retail.sales_stream;
--- CREATE TABLE bronze_db_retail.sales_stream as
--- select * from json.`dbfs:/databricks-datasets/retail-org/sales_stream/sales_stream.json`;
+drop table if exists bronze_db_retail.sales_stream;
+CREATE TABLE bronze_db_retail.sales_stream as
+select * from json.`dbfs:/databricks-datasets/retail-org/sales_stream/sales_stream.json`;
 
 --10
--- drop table if exists bronze_db_retail.suppliers;
--- drop view if exists suppliers;
--- CREATE OR REPLACE TEMP VIEW suppliers
--- --(loyalty_segment_id string, loyalty_segment_description string, unit_threshold string, valid_from string, valid_to string )
--- USING CSV
--- OPTIONS (
---   path "dbfs:/mnt/dbacademy-datasets/data-engineering-with-databricks/v02/retail-org/suppliers/suppliers.csv",
---   header "true"
---   --delimiter ";"
---   );
--- CREATE TABLE bronze_db_retail.suppliers AS
---   SELECT * FROM suppliers;
+drop table if exists bronze_db_retail.suppliers;
+drop view if exists suppliers;
+CREATE OR REPLACE TEMP VIEW suppliers
+--(loyalty_segment_id string, loyalty_segment_description string, unit_threshold string, valid_from string, valid_to string )
+USING CSV
+OPTIONS (
+  path "dbfs:/mnt/dbacademy-datasets/data-engineering-with-databricks/v02/retail-org/suppliers/suppliers.csv",
+  header "true"
+  --delimiter ";"
+  );
+CREATE TABLE bronze_db_retail.suppliers AS
+  SELECT * FROM suppliers;
 
 -- COMMAND ----------
 
@@ -185,38 +179,6 @@ describe database bronze_db_retail;
 --select * from bronze_db_retail.suppliers;
 --select * from bronze_db_retail.purchase_orders;-- where EAN13=2198122550193;
 DESCRIBE EXTENDED bronze_db_retail.purchase_orders;
-
--- COMMAND ----------
-
-DESCRIBE EXTENDED bronze_db_retail.sales_orders;
-
--- COMMAND ----------
-
--- MAGIC %python
--- MAGIC # dbutils.fs.head("dbfs:/mnt/dbacademy-datasets/data-engineering-with-databricks/v02/retail-org/loyalty_segments/loyalty_segment.csv")
--- MAGIC p="/dbfs/mnt/dbacademy-datasets/data-engineering-with-databricks/v02/retail-org/sales_stream/sales_stream.json/"
--- MAGIC # f = open(p, "r")
--- MAGIC # print(f)
--- MAGIC 
--- MAGIC with  open(p, "r") as f_read:
--- MAGIC   for line in f_read:
--- MAGIC     print(line)
--- MAGIC spark_p="dbfs:/mnt/dbacademy-datasets/data-engineering-with-databricks/v02/retail-org/sales_stream/"
--- MAGIC # df = spark.read.option("header","true").option("delimiter",";").json(spark_p)
--- MAGIC # display(df)
--- MAGIC # print(df.dtypes)
-
--- COMMAND ----------
-
--- MAGIC %md
--- MAGIC 
--- MAGIC  
--- MAGIC Run the following cell to delete the tables and files associated with this lesson.
-
--- COMMAND ----------
-
--- MAGIC %python
--- MAGIC DA.cleanup()
 
 -- COMMAND ----------
 
